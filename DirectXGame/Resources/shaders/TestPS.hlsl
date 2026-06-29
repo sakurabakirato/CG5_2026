9725a9436abe87f3bc4d87429fc8ffbc9dc20146
sakurabakirato <k024g0054@g.neec.ac.jp>
2026-06-29 12:44:37 +0900

    00_08完了

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -1,11 +1,18 @@
+#include "Test.hlsli"
+
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
 };
 
-PixelShaderOutput main()
+PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
-    output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
+    
+    float2 uv = input.texcoord;
+
+    // uv(x,y)を色(r,g)として表示する
+    output.color = float4(uv.x, uv.y, 0.0f, 1.0f);
+
 	return output;
 }
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -18,11 +18,16 @@ void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader&
 {
 
 	// InputLayout --------------------
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
 	inputElementDescs[0].SemanticName = "POSITION";
 	inputElementDescs[0].SemanticIndex = 0;
 	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+
+	inputElementDescs[1].SemanticName = "TEXCOORD";
+	inputElementDescs[1].SemanticIndex = 0;
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
 
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;
@@ -105,15 +110,16 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 	struct VertexData 
 	{
 		Vector4 position;
+		Vector2 texcoord;
 	};
 
 	// 頂点データの準備
 	VertexData vertices[] = 
 	{
-	    {-1.0f, 1.0f,  0.0f, 1.0f}, // 左上
-	    {1.0f,  1.0f,  0.0f, 1.0f}, // 右上
-	    {-1.0f, -1.0f, 0.0f, 1.0f}, // 左下
-	    {1.0f,  -1.0f, 0.0f, 1.0f}, // 右下
+	    {{-1.0f, 1.0f, 0.0f, 1.0f},  {0.0f, 0.0f}}, // 左上
+	    {{1.0f, 1.0f, 0.0f, 1.0f},   {1.0f, 0.0f}}, // 右上
+	    {{-1.0f, -1.0f, 0.0f, 1.0f}, {0.0f, 1.0f}}, // 左下
+	    {{1.0f, -1.0f, 0.0f, 1.0f},  {1.0f, 1.0f}}, // 右下
 	};
 
 	// 頂点インデックスデータの準備
