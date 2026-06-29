f6f8541117d648a7c59828da81b6fafeb4999447
sakurabakirato <k024g0054@g.neec.ac.jp>
2026-06-29 12:13:46 +0900

    00_07完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -108,19 +108,31 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 	};
 
 	// 頂点データの準備
+	//VertexData vertices[] = 
+	//{
+	//    {0.0f, 0.5f, 0.0f, 1.0f},   // 上
+	//    {0.5f, -0.5f, 0.0f, 1.0f},  // 右下
+	//    {-0.5f, -0.5f, 0.0f, 1.0f}, // 左下
+	//};
 	VertexData vertices[] = 
 	{
-	    {0.0f, 0.5f, 0.0f, 1.0f},   // 上
-	    {0.5f, -0.5f, 0.0f, 1.0f},  // 右下
-	    {-0.5f, -0.5f, 0.0f, 1.0f}, // 左下
+	    {-1.0f, 1.0f,  0.0f, 1.0f}, // 左上
+	    {1.0f,  1.0f,  0.0f, 1.0f}, // 右上
+	    {-1.0f, -1.0f, 0.0f, 1.0f}, // 左下
+	    {1.0f,  -1.0f, 0.0f, 1.0f}, // 右下
 	};
 
 	// 頂点インデックスデータの準備
-	uint16_t indices[] = 
+	/*uint16_t indices[] = 
 	{
 	    0,
 	    1,
 	    2,
+	};*/
+	uint16_t indices[] = 
+	{
+	    0, 1, 2, 
+		1, 3, 2,
 	};
 
 	// IndexBuffer(IndexResource、IndexResourceView)の生成
